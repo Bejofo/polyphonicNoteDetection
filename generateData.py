54bbed86f526f1b0cd3713533af1139015625fb8
Bejofo <32919220+Bejofo@users.noreply.github.com>
2021-02-13 14:05:51 +0800

    Change activation functions, optimizer& reduced noise

--- generateData.py
+++ generateData.py
@@ -2,22 +2,34 @@ import librosa
 import numpy as np 
 import random
 import time
+import soundfile
+
+#unused, and proablly wrong
+def unOneHotEncode(vec):
+    ans = []
+    for k,v in enumerate(vec):
+        if math.isclose(v,1):
+            ans.append(k)
+    return ans
 
 
 def generateTrainingData():
-	num_of_notes = random.randint(0,3)
+	num_of_notes = random.randint(1,3)
 	sample_rate = 22050
 	waveform = np.zeros(sample_rate) # a second of audio
 	label = np.zeros(128)
+	human_readable_label = []
 	notes = random.sample(range(1, 128), num_of_notes)
 	for n in notes:
 		hertz = librosa.midi_to_hz(n)
 		waveform += librosa.tone(hertz, sr=sample_rate, length=sample_rate)
 		label += oneHotEncode(n)		
+		human_readable_label.append(librosa.midi_to_note(n))
 	# sprinkle in some noise
-	waveform += np.random.rand(sample_rate) * 10
+	waveform += (np.random.rand(sample_rate)-0.5) * 0.01
+	waveform/=num_of_notes # otherwise it clips
 	freqs = librosa.feature.melspectrogram(y=waveform, sr=sample_rate)
-	return freqs[:,1],label
+	return freqs[:,1],label,human_readable_label,waveform
 
 def oneHotEncode(n):
 	ans = np.zeros(128)
@@ -34,8 +46,11 @@ if __name__ == "__main__":
     print("How many samples to generate?")
     i = int(input())
     for x in range(i):
-        data,l=generateTrainingData()
+        data,l,human,waveform=generateTrainingData()
         trainingData = np.concatenate((trainingData, [data]))
         labels = np.concatenate((labels,[l]))
+        if x % 100 == 0:
+          soundfile.write(f"{human}.wav", waveform, 22050, subtype='PCM_24')
+          print(x)
     np.savetxt("trainingData.txt",trainingData,fmt='%.5e')
     np.savetxt("labels.txt",labels,fmt='%.1e')
--- train.py
+++ train.py
@@ -1,22 +1,20 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import SGD
+import keras
 import numpy as np 
 
 X = np.loadtxt("trainingData.txt")
 y = np.loadtxt("labels.txt")
 
 model = Sequential()
-model.add(Dense(128, input_dim=128))
-model.add(Activation('tanh'))
+model.add(Dense(256, input_dim=128))
+model.add(Activation('relu'))
 model.add(Dense(128))
 model.add(Activation('sigmoid'))
 
-
-sgd = SGD(lr=0.1)
-model.compile(loss='binary_crossentropy', optimizer=sgd)
-
-model.fit(X, y, batch_size=1, epochs=1000)
-model.save("firstmodel")
+opt = keras.optimizers.Adam(lr=0.001)
+model.compile(loss='binary_crossentropy', optimizer=opt,metrics=['accuracy'])
+model.fit(X, y, batch_size=64, epochs=100)
+model.save("secondmodel")
 pred = model.predict(X)
 np.savetxt("prediction.txt",pred,fmt="%1.2f")
