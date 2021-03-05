89c14c0d39a8b528f2c726a8b7f32bded8de32f0
Bejofo <32919220+Bejofo@users.noreply.github.com>
2021-03-05 12:27:54 +0800

    Update generateData.py

--- generateData.py
+++ generateData.py
@@ -1,56 +1,65 @@
+from multiprocessing import Pool
+from SampleBank import SampleBank 
+import os
 import librosa
-import numpy as np 
 import random
-import time
-import soundfile
+import numpy as np
+import cProfile
+import functools
 
-#unused, and proablly wrong
-def unOneHotEncode(vec):
-    ans = []
-    for k,v in enumerate(vec):
-        if math.isclose(v,1):
-            ans.append(k)
-    return ans
+filenames = os.listdir("samples")
+sounds = SampleBank()
 
-
-def generateTrainingData():
-	num_of_notes = random.randint(1,3)
-	sample_rate = 22050
-	waveform = np.zeros(sample_rate//8) # a second of audio
-	label = np.zeros(128)
-	human_readable_label = []
-	notes = random.sample(range(0, 128), num_of_notes)
-	for n in notes:
-		hertz = librosa.midi_to_hz(n)
-		waveform += librosa.tone(hertz, sr=sample_rate, length=sample_rate//8)
-		label += oneHotEncode(n)		
-		human_readable_label.append(librosa.midi_to_note(n))
-	# sprinkle in some noise
-	waveform += (np.random.rand(sample_rate//8)-0.5) * 0.01
-	waveform/=num_of_notes # otherwise it clips
-	freqs = librosa.feature.melspectrogram(y=waveform, sr=sample_rate)
-	return freqs[:,1+random.randint(1,4)],label,human_readable_label,waveform
-
+@functools.lru_cache(maxsize=None)
 def oneHotEncode(n):
 	ans = np.zeros(128)
 	ans[n] = 1
-	return ans 
+	return np.array(ans) 
 
-trainingData = np.array([np.zeros(128)])
-labels = np.array([np.zeros(128)])
 
-if __name__ == "__main__":
-    SEED = int(time.time())
-    random.seed(SEED)
-    print(f"Using {SEED} as seed")
-    print("How many samples to generate?")
-    i = int(input())
-    for x in range(i):
-        data,l,human,waveform=generateTrainingData()
-        trainingData = np.concatenate((trainingData, [data]))
-        labels = np.concatenate((labels,[l]))
-        if x % 100 == 0:
-          #soundfile.write(f"{human}.wav", waveform, 22050, subtype='PCM_24')
-          print(x)
-    np.savetxt("trainingData.txt",trainingData,fmt='%.5e')
-    np.savetxt("labels.txt",labels,fmt='%.1e')
+@functools.lru_cache(maxsize=None)
+def filename_to_note(f):
+	return f.split("-")[0]
+
+def generateExample(_):
+    filesToUse = []
+    sr = 22050
+    for _ in range(random.randint(1,2)):
+	    filesToUse.append(random.choice(filenames))
+    filesToUse = list(set(filesToUse))
+    label = []
+    for filename in filesToUse:
+        noteName = filename_to_note(filename)
+        noteName= librosa.note_to_midi(noteName)
+        if noteName < 25 or noteName > 108:
+          return generateExample(_)
+        label.append(oneHotEncode(noteName))
+    label = sum(label)
+    if max(label) == 2:
+        return generateExample(None)
+    #wave = sum(sounds.raw[filename][:2048] for filename in filesToUse)
+    freqs = abs(sum(sounds.cqt[filename][:,random.randint(0,6)] for filename in filesToUse))
+    #features = np.concatenate((freqs,wave))
+    return freqs,label
+
+
+if __name__ =="__main__":
+    i = int(input("Examples to generate?"))
+    trainingData = np.zeros((i,84*5))
+    labels =  np.zeros((i,128))
+    pool = Pool()
+    for d,l in pool.imap(generateExample,range(i)):
+        trainingData[i-1] = np.array(d)
+        labels[i-1] = np.array(l)
+        i-=1
+        # if i%200==0:
+            # print(i)
+    pool.close()
+    pool.join()
+    try:
+        trainingData = np.vstack(np.load("trainingData.npy"),trainingData)
+        labels =  np.vstack(np.load("labels.npy"),labels)
+    except:
+        pass
+    np.save("trainingData.npy",trainingData)
+    np.save("labels.npy",labels)
